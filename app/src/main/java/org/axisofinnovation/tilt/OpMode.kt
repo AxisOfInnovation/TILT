87f42103e54ae65d7e9391d1a6d72152d78ed3c4
donofanne <austin.donovan501@gmail.com>
2016-02-21 19:00:58 -0600

    Added deferred Hardware setting
    No longer will one have to do the stupid
    "= hardwareMap.<whatever>.get( "name" )"
    stuff!
    Now, providing that you extend the new type of OpMode, it will automatically be
    done to any field annotated with the @Hardware annotation.

--- app/src/main/java/org/axisofinnovation/tilt/OpMode.kt
+++ app/src/main/java/org/axisofinnovation/tilt/OpMode.kt
@@ -1,10 +1,35 @@
 package org.axisofinnovation.tilt
 
+import org.axisofinnovation.tilt.config.configure
+import org.axisofinnovation.tilt.hardware.fetchHardware
+
 /**
  * An extension of Qualcomm's OpMode class, this provides additional functionality that
  * may not have been possible before.
  */
 abstract class OpMode : com.qualcomm.robotcore.eventloop.opmode.OpMode()
 {
+
+    /**
+     * If you are looking to do initialization code, please
+     * do it in the `initialize` method supplied by this class.
+     */
+    final override fun init()
+    {
+        // do the things we need to do
+        fetchHardware( this );
+        configure( OpMode::class.java, "" );
+
+        // actually initialize the robot
+        initialize();
+    }
+
+    //
+    // Abstract
+    //
+
+    abstract fun initialize();
+
+    abstract override fun loop();
 
 }
--- app/src/main/java/org/axisofinnovation/tilt/hardware/HardwareFetcher.kt
+++ app/src/main/java/org/axisofinnovation/tilt/hardware/HardwareFetcher.kt
@@ -2,12 +2,65 @@ package org.axisofinnovation.tilt.hardware
 
 import android.util.Log
 import com.qualcomm.robotcore.eventloop.opmode.OpMode
+import com.qualcomm.robotcore.hardware.HardwareMap
 import org.axisofinnovation.tilt.utils.FieldFilter
+import java.lang.reflect.Field
+import java.lang.reflect.ParameterizedType
 
-fun fetchHardware( clazz: Class< OpMode > )
+/**
+ * Fetches all of the hardware by the names in their annotations
+ *
+ * @param opMode
+ *          The opmode to search hardware for.
+ */
+fun fetchHardware( opMode: OpMode )
 {
-    Log.d( "tilt.HardwareFetcher", "Fetching hardware in ${clazz.simpleName}" );
+    Log.d( "tilt.HardwareFetcher", "Fetching hardware in ${opMode.javaClass.simpleName}" );
 
     // all the hardware fields
-    val fieldFilter = FieldFilter( clazz ).with( Hardware::class.java );
+    val fieldFilter = FieldFilter( opMode ).with( Hardware::class.java );
+
+    // all the hardware maps in the hardware class
+    val hardwareMaps = FieldFilter( opMode.hardwareMap ).getFieldsOfType( HardwareMap.DeviceMapping::class.java );
+
+    // loop over each DeviceMapping
+    for ( field in hardwareMaps )
+    {
+        // get the actual field value
+        val map = field.get( opMode.hardwareMap ) as HardwareMap.DeviceMapping< * >;
+
+        fetch( opMode, map, fieldFilter );
+    }
+}
+
+/**
+ * Actually sets the value of the field.
+ *
+ * @param opMode
+ *          The OpMode we're modifying
+ * @param list
+ *          The DeviceMapping we're getting values from.
+ * @param fieldFilter
+ *          Used to find fields only matching the same type of DeviceMapping we're accessing.
+ */
+private fun fetch( opMode: OpMode, list: HardwareMap.DeviceMapping< * >, fieldFilter: FieldFilter )
+{
+    // gets the fields of the same type in the device mapping list
+    // yes, this is terrible
+    // I think it works
+    val fields = fieldFilter.getFieldsOfType( ( list.javaClass.genericSuperclass as ParameterizedType ).actualTypeArguments[ 0 ].javaClass );
+
+    // set the value of each field to the correct name
+    fields.forEach { f -> f.set( opMode, list.get( getHardwareName( f ) ) ) };
+}
+
+/**
+ * @return The value of the configName field in the Annotation if it was set, otherwise
+ *         just the field name.
+ */
+fun getHardwareName( field: Field ): String
+{
+    val configName = field.getAnnotation( Hardware::class.java ).configName;
+    if ( !configName.equals( " " ) ) return configName;
+    return field.name;
 }
